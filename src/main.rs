8f3a72b18e3f8813b17bd30b297aa076d6bab2bf
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2024-09-19 00:36:23 -0400

    Hmmm maybe? CFG start

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 
 mod lexer;
 
+struct grammar {
+    rules: Vec<production_rule>, // Replace with set
+    // Get terms or non terms?
+}
+
+struct production_rule {
+    left: nonterminal,
+    right: Option<Vec<symbol>>,
+}
+
+enum symbol {
+    terminal,
+    nonterm,
+}
+
+struct termial {
+    value: String,
+}
+
+struct nonterminal {
+    identifier: String,
+}
 fn main() {
+    let nonterm_A: nonterminal = nonterminal {identifier: "A".to_string()};
+
+    let mut test = grammar {rules: vec![
+        production_rule {left: nonterm_A, right: None},
+    ]};
 
 }
